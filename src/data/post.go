b712dddeec3723d3bd9bea2df32684fef9fcf362
amzking <amzking@foxmail.com>
2018-09-07 23:13:42 +0800

    y

--- src/data/post.go
+++ src/data/post.go
@@ -14,3 +14,10 @@ type Post struct {
 func (post *Post) CreateAtDate() string {
 	return post.CreatedAt.Format("Jan 2, 2018 at 10.58pm")
 }
+
+func (post *Post) User() (user User) {
+	user = User{}
+	Db.QueryRow("SELECT id, uuid, name, email, created_at FROM users WHERE id = $1", post.UserId).
+		Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.CreatedAt)
+	return
+}
--- src/data/user.go
+++ src/data/user.go
@@ -22,3 +22,15 @@ func (user *User) CreateThread(topic string) (conv Thread, err error) {
 	err = stmt.QueryRow(createUUID(), topic, user.Id, time.Now()).Scan(&conv.Id, &conv.Uuid, &conv.Topic, &conv.UserId, &conv.CreatedAt)
 	return
 }
+
+func (user *User) CreatePost(conv Thread, body string) (post Post, err error) {
+	statement := "insert into posts (uuid, body, user_id, thread_id, created_at) values ($1, $2, $3, $4, $5) returning id, uuid, body, user_id, thread_id, created_at"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	// use QueryRow to return a row and scan the returned id into the Session struct
+	err = stmt.QueryRow(createUUID(), body, user.Id, conv.Id, time.Now()).Scan(&post.Id, &post.Uuid, &post.Body, &post.UserId, &post.ThreadId, &post.CreatedAt)
+	return
+}
